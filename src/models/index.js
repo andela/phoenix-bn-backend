e6a19389d6b55c7c58b2990c8d6a2fb6dfa06924
ugwumadu116 <ugwumadu116@gmail.com>
2019-08-22 16:28:18 +0100

    fix hund import issues

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const config = require(`${__dirname}/../database/config.js`)[env];
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '/../database/config.js'))[env];
-
-
 const db = {};
 let sequelize;
 if (config.use_env_variable) {
@@ -16,7 +15,7 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
