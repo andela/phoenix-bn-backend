a15f19929d22f947915918679ba2a4958af46c1c
ugwumadu116 <ugwumadu116@gmail.com>
2019-08-22 15:58:43 +0100

    fix newline issues

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,7 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     content: DataTypes.STRING,
     complete: DataTypes.BOOLEAN
   }, {});
-  User.associate = function (models) {
-    // associations can be defined here
-  };
   return User;
 };
--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,12 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../database/config.js')[env];
-const db = {};
+const config = require(path.join(__dirname, '/../database/config.js'))[env];
 
+
+const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -17,11 +16,9 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter((file) => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
